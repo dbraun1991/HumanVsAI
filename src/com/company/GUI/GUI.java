91751dcfe1ad7f05962ed40f3ab44dc0bb1c6fda
dbraun3 <d.braun3@smail.uni-koeln.de>
2017-11-10 16:25:14 +0100

    Modified menue structure + Bugfix.

--- src/com/company/GUI/GUI.java
+++ src/com/company/GUI/GUI.java
@@ -1,15 +1,20 @@
 package com.company.GUI;
 
+import com.company.AI.Neuron;
+import com.company.AI.Pool;
+import com.company.AI.Species;
+import com.company.Game.Game;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-
+import java.util.LinkedList;
 
 
 public class GUI {
 
     public static JLabel myView;
-    public BufferedImage mySurface;
+    private BufferedImage mySurface;
 
     public int myWidth=0;
     public int myHeight=0;
@@ -40,13 +45,86 @@ public class GUI {
 
 
     public void UpdateGui () {
-        // repaint
+        // Repaint
         Graphics g = mySurface.getGraphics();
 
-        // background
+        // Background
         g.setColor(Color.darkGray);
         g.fillRect(0,0, this.myWidth, this.myHeight);
     }
+
+
+
+    public void pntAIArea (Game myGame, LinkedList<Neuron> [] AI, Pool pool) {
+        // Repaint
+        Graphics g = mySurface.getGraphics();
+
+        // Background
+        g.setColor(Color.darkGray);
+        g.fillRect(250,150, this.myWidth-500, this.myHeight-300);
+        g.setColor(Color.black);
+        g.fillRect(250,150, this.myWidth-500, this.myHeight-500);
+
+        // game
+        myGame.showGameState();
+
+        // temp
+        pntIdeas(g);
+
+        // show AI
+        if (!(AI == null)) {
+            // paint that
+
+            // low layer to top layer
+            int layers = AI.length;
+            double [][] gameWindow = myGame.getGameWindow();
+
+            if (!(AI == null)) {
+                Neuron [] input = pool.input;
+
+
+
+            }
+            // done
+        }
+
+    }
+
+
+
+    private void pntIdeas (Graphics g) {
+
+        int X = 300;
+        int Y = 200;
+
+        int tilesize = 21;
+        int linksize = 5;
+
+        int linkX = 0;
+        int linkY = 0;
+
+        g.setColor(Color.lightGray);
+        g.fillRect(X,Y, tilesize, tilesize);
+
+        g.setColor(Color.red);
+        g.fillRect(X+1+((1+linkX)*3)+(linksize*linkX),Y+1+((1+linkY)*3)+(linksize*linkY), linksize, linksize);
+
+        linkX = 1;
+        g.setColor(Color.yellow);
+        g.fillRect(X+1+((1+linkX)*3)+(linksize*linkX),Y+1+((1+linkY)*3)+(linksize*linkY), linksize, linksize);
+
+        linkX = 0;
+        linkY = 1;
+        g.setColor(Color.blue);
+        g.fillRect(X+1+((1+linkX)*3)+(linksize*linkX),Y+1+((1+linkY)*3)+(linksize*linkY), linksize, linksize);
+
+        linkX = 1;
+        g.setColor(Color.green);
+        g.fillRect(X+1+((1+linkX)*3)+(linksize*linkX),Y+1+((1+linkY)*3)+(linksize*linkY), linksize, linksize);
+
+    }
+
+
 
     public BufferedImage getSurface() {
         return mySurface;
@@ -55,3 +133,5 @@ public class GUI {
 
 
 }
+
+
